5959ae5388e0cf47a1bd59656c749c5350bfe450
nonybright <nonybrighto@gmail.com>
2019-05-15 18:15:33 +0100

    Modify drawer

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,44 +1,22 @@
 <template>
   <v-app dark>
-    <v-navigation-drawer
-      v-model="drawer"
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      fixed
-      app
-    >
-      <DrawerItems />
-    </v-navigation-drawer>
-    <v-toolbar :clipped-left="clipped" fixed app>
-      <v-toolbar-side-icon @click="drawer = !drawer" />
-      <v-toolbar-title v-text="title" />
-      <v-spacer />
-    </v-toolbar>
+    <Header />
     <v-content>
       <v-container>
         <nuxt />
       </v-container>
     </v-content>
-    <v-footer :fixed="fixed" app>
+    <v-footer :fixed="true" app>
       <span>&copy; 2019</span>
     </v-footer>
   </v-app>
 </template>
 
 <script>
-import DrawerItems from '../components/DrawerItems'
+import Header from '../components/Header'
 export default {
   components: {
-    DrawerItems
-  },
-  data() {
-    return {
-      clipped: false,
-      drawer: false,
-      fixed: false,
-      miniVariant: false,
-      title: 'nonybrighto.com'
-    }
+    Header
   }
 }
 </script>
