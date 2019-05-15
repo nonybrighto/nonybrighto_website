932ae99844636c50a4eae9c238ae5ed2d5c017bb
nonybright <nonybrighto@gmail.com>
2019-05-15 21:13:58 +0100

    create portfolio page

---
+++ components/Project.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-card class="ma-1">
+    <v-img :src="require('~/assets/images/image.png')" height="260px"></v-img>
+    <v-card-title>
+      <div>{{ project.name }}</div>
+      <div class="grey--text">{{ project.description }}</div>
+    </v-card-title>
+    <v-divider light></v-divider>
+    <v-card-actions class="pa-3">
+      <v-chip color="blue">Mobile</v-chip>
+      <v-spacer></v-spacer>
+      <div>
+        <v-btn flat color="primary">Primary</v-btn>
+      </div>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: {
+    project: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
--- pages/portfolio/_tag/index.vue
+++ pages/portfolio/_tag/index.vue
@@ -1,15 +1,149 @@
 <template>
-  <v-layout>
-    <v-flex text-xs-center>
-      <img src="/v.png" alt="Vuetify.js" class="mb-5" />
-      <blockquote class="blockquote">
-        &#8220;These are the content for this tag .&#8221;
-        <footer>
-          <small>
-            <em>&mdash;John Johnson</em>
-          </small>
-        </footer>
-      </blockquote>
-    </v-flex>
+  <v-layout column align-center>
+    <h2>Portfolio</h2>
+    <p>These are the projects that have been worked on recently</p>
+    <v-layout row>
+      <v-chip
+        v-for="platform in platforms"
+        :key="platform.id"
+        :color="platform.color"
+        text-color="white"
+      >
+        <v-avatar>
+          <v-icon>{{ platform.icon }}</v-icon>
+        </v-avatar>
+        {{ platform.name }}
+      </v-chip>
+    </v-layout>
+    <v-layout row wrap>
+      <v-chip v-for="tag in tags" :key="tag.id" text-color="white">
+        {{ tag.name }}
+      </v-chip>
+    </v-layout>
+    <v-container fluid>
+      <v-layout row wrap>
+        <v-flex v-for="project in projects" :key="project.id" xs12 sm6 md4>
+          <Project :project="project" />
+        </v-flex>
+      </v-layout>
+    </v-container>
   </v-layout>
 </template>
+
+<script>
+import Project from '~/components/Project.vue'
+export default {
+  components: {
+    Project
+  },
+  data() {
+    return {
+      happy: true,
+      platforms: [
+        {
+          id: 'web',
+          name: 'Web',
+          icon: 'account_circle',
+          color: 'pink'
+        },
+        {
+          id: 'mobile',
+          name: 'Mobile',
+          icon: 'account_circle',
+          color: 'purple'
+        },
+        {
+          id: 'desktop',
+          name: 'Desktop',
+          icon: 'account_circle',
+          color: 'blue'
+        }
+      ],
+
+      tags: [
+        {
+          id: 'web',
+          name: 'css'
+        },
+        {
+          id: 'web',
+          name: 'css'
+        },
+        {
+          id: 'html',
+          name: 'html'
+        },
+        {
+          id: 'flutter',
+          name: 'flutter'
+        },
+        {
+          id: 'javaFX',
+          name: 'javaFX'
+        },
+        {
+          id: 'electron',
+          name: 'electron'
+        },
+        {
+          id: 'unity',
+          name: 'unity'
+        },
+        {
+          id: 'vue',
+          name: 'vue'
+        }
+      ],
+
+      projects: [
+        {
+          id: '1',
+          name: 'Project name',
+          description: 'THis is a very short descrition of the project',
+          iconImagePath: '../../assets/images/image.png',
+          sourceLink: '',
+          imagePaths: [''],
+          tags: ['']
+        },
+        {
+          id: '2',
+          name: 'Project name',
+          description: 'THis is a very short descrition of the project',
+          iconImagePath: '/assets/images/image.png',
+          sourceLink: 'no link',
+          imagePaths: [''],
+          tags: ['']
+        },
+        {
+          id: '3',
+          name: 'Project name',
+          description: 'THis is a very short descrition of the project',
+          iconImagePath: '/assets/images/image.png',
+          sourceLink: '',
+          imagePaths: [''],
+          tags: ['']
+        },
+        {
+          id: '4',
+          name: 'Project name',
+          description: 'THis is a very short descrition of the project',
+          iconImagePath: '~/images/image.png',
+          sourceLink: '',
+          imagePaths: [''],
+          tags: ['']
+        },
+        {
+          id: '5',
+          name: 'Project name',
+          description: 'THis is a very short descrition of the project',
+          iconImagePath:
+            'https://www.gettyimages.co.uk/gi-resources/images/RoyaltyFree/Apr17Update/ColourSurge1.jpg',
+          sourceLink: '',
+          imagePaths: [''],
+          tags: ['']
+        }
+      ]
+    }
+  }
+}
+</script>
